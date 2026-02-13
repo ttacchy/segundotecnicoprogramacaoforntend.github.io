20f82049ec827aba6f730c625c6899b448bb5e3d
tacchy <ttacchy@gmail.com>
2026-02-13 11:24:53 -0300

    Add button click event with console log and alert

---
+++ script.js
@@ -0,0 +1,11 @@
+// Seleciona o botão pelo id
+const botao = document.getElementById("botao");
+
+// Adiciona um evento de clique ao botão
+botao.addEventListener("click", function () {
+    // Mostra uma mensagem no console
+    console.log("Botão clicado!");
+
+    // Mostra um alerta na tela
+    alert("Você clicou no botão!");
+});
